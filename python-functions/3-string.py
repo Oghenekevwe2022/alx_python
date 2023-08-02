8e32731b0efae6485ccb3598ecda6770928de153
Udi <elijahudi@gmail.com>
2023-08-02 09:24:17 +0100

    tring Manipulation Function

---
+++ python-functions/3-string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# def reverse_string(string):
+#     return 
+
+number_string = input("Enter number: ")
+number_string_reversed = number_string[::-1]
+
+# print(number_string_reversed)
