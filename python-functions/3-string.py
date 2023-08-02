794e48b1710d43e407d927ffd396dbf11e8a8380
Udi <elijahudi@gmail.com>
2023-08-02 09:50:27 +0100

    String Manipulation Function

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -2,10 +2,3 @@
 
 def reverse_string(string):
     return string [::-1]
-
-# Hello = input('Enter word: ')
-
-# number_string = input("Enter number: ")
-# number_string_reversed = number_string[::-1]
-
-print(reverse_string("Hello, World"))
