41cc9c0df802d161652f1a4f153462f04d864948
Udi <elijahudi@gmail.com>
2023-08-05 22:48:21 +0100

    How to make a script dynamic

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,6 @@
+def no_of_argu(*args):
+        return(len(args))
+if __name__ == "__main__":
+    print(no_of_argu("Hello", "Holbert", "Fifa", "Gabril", "Peter", "James"))
+    print(no_of_argu(2,4,"c",4,6))
+
