6b0e819b1e767c17e0a971039a88528c538fc7c2
Udi <elijahudi@gmail.com>
2023-08-05 03:18:11 +0100

    0. Import a simple function from a simple file

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 
-def validate_password(password):
-    if (len(password) >= 8):
+# def validate_password(password):
+#     if (len(password) >= 8):
     
-        lowerCase = False
-        upperCase = False
-        num = False
-        space = True
+#         lowerCase = False
+#         upperCase = False
+#         num = False
+#         space = True
 
-        for char in password:
-            if(char.isdigit()):
-                num = True
-            if(char.islower()):
-                lowerCase = True
-            if(char.isupper()):
-                upperCase = True
-            if(not char.isalnum()):
-                space = False
+#         for char in password:
+#             if(char.isdigit()):
+#                 num = True
+#             if(char.islower()):
+#                 lowerCase = True
+#             if(char.isupper()):
+#                 upperCase = True
+#             if(not char.isalnum()):
+#                 space = False
 
-        return lowerCase and upperCase and num and space
-    else:
-        return False
+#         return lowerCase and upperCase and num and space
+#     else:
+#         return False
+
+def my_function(counter=89):
+    return counter + 1
+print(my_function())
     
         
