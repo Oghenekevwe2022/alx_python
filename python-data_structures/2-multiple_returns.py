9c0052b3ce04c4acb0a4bdb9843a48f2d12b6e71
Udi <elijahudi@gmail.com>
2023-08-13 10:23:20 +0100

    2. More returns

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    multiple_returns = __import__('2-multiple_returns').multiple_returns
+    if not sentence:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    length = len(sentence)
+    return length, first_char
+
