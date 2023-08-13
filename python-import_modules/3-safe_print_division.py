325492fada739f489111bdfc97891e4dcc95b8ed
Udi <elijahudi@gmail.com>
2023-08-13 01:10:26 +0100

    Changes

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 
 def safe_print_division(a, b):
     safe_print_division = __import__('3-safe_print_division').safe_print_division
-    return a/b
+    return (a/b)
 
 try:
     a = 12
@@ -9,9 +10,13 @@ try:
     result = safe_print_division(a, b)
     # print("{:d} / {:d} = {}".format(a, b, result))
 except:
-     print("{:d} / {:d} = {}".format(a, b, result))
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("Inside result: {:d} / {:d} = {}".format(a, b, result))
 finally:
-    print(safe_print_division(12, 2))
+#     print(safe_print_division(12, 2))
+    print("Inside result: {:d} / {:d} = {}".format(a, b, result))
 
 
 
@@ -23,13 +28,6 @@ finally:
 
 
 
-# try:
-#   f = open("demofile.txt")
-#   try:
-#     f.write("Lorum Ipsum")
-#   except:
-#     print("Something went wrong when writing to the file")
-#   finally:
-#     f.close()
-# except:
-#   print("Something went wrong when opening the file")
+
+
+
